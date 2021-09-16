5e59b7c5742736fa54a04e2f348fc597559c9f40
Jorge Alonso Ayala <Jorgealonsoa@gmail.com>
2021-09-15 21:21:14 -0500

    Commiting main.tf

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aci = {
+      source = "ciscodevnet/aci"
+    }
+  }
+}
+
+#configure provider with your cisco aci credentials.
+provider "aci" {
+  # cisco-aci user name
+  username = "admin"
+  # cisco-aci password
+  password = "password"
+  # cisco-aci url
+  url      = "https://my-cisco-aci.com"
+  insecure = true
+}
+
+resource "aci_tenant" "test-tenant" {
+  name        = "test-tenant"
+  description = "This tenant is created by terraform"
+}
+
+resource "aci_application_profile" "test-app" {
+  tenant_dn   = "${aci_tenant.test-tenant.id}"
+  name        = "test-app"
+  description = "This app profile is created by terraform"
+}
+
